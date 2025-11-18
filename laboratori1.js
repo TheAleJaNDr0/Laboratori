9e4075dce56ba4562ad4182a47aab413c63911ad
Alejandro Gómez <alejandrogomezmas@gmail.com>
2025-11-18 16:14:58 +0000

    activity 4 done

--- laboratori1.js
+++ laboratori1.js
@@ -1,1 +1,16 @@
 console.log("Hola, món!");
+
+// 4
+let numero = 12;
+let text = "Hola";
+const ciutat = "Barcelona";
+let boolea = true;
+
+let concatenacio = text + " món! Visc a " + ciutat;
+
+let interpolacio = `${text} món! Visc a ${ciutat} i el número és ${numero}`;
+
+console.log(concatenacio);
+console.log(interpolacio);
+
+//5
