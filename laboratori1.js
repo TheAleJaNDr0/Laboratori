df7d384acb75b7227b5cf549bdaad5ea9abb1d57
Alejandro Gómez <alejandrogomezmas@gmail.com>
2025-11-18 16:24:08 +0000

    activity 6 done

--- laboratori1.js
+++ laboratori1.js
@@ -25,3 +25,45 @@ console.log("Estudiant:", estudiant);
 console.log("Valor nul:", valorNul);
 
 console.log(`El meu nom es ${nom}, tinc ${edat} anys i és ${estudiant} que estic estudiant.`);
+
+// 6
+let a = 15;
+let b = 4;
+
+let suma = a + b;
+let resta = a - b;
+let multiplicacio = a * b;
+let divisio = a / b;
+let residu = a % b;
+
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicació:", multiplicacio);
+console.log("Divisió:", divisio);
+console.log("Residu:", residu);
+
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+console.log("a != b:", a != b);
+console.log("a !== b:", a !== b);
+console.log("a < b:", a < b);
+console.log("a > b:", a > b);
+// 7
+
+// 8 
+
+// 9
+
+// 10
+
+// 11
+
+// 12
+
+// 13
+
+// 14
+
+// 15
+
+// 16
